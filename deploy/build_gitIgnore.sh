f11e24daac0a2fac9230a72313d1b777f39a5504
apan1121 <apan1121@gmail.com>
2020-02-16 14:41:26 +0800

    [fix]

--- deploy/build_gitIgnore.sh
+++ deploy/build_gitIgnore.sh
@@ -2,6 +2,7 @@ MODE_TYPE=$1
 
 PUBLIC_GIT_IGNORE_PATH="$ABS_DIR/.gitignore"
 
+
 if [ "$MODE_TYPE" == "dev" ]; then
     echo "設定 git ignore dev 模式";
 
@@ -11,7 +12,12 @@ package-lock.json
 " > $PUBLIC_GIT_IGNORE_PATH
 else
     echo "設定 git ignore prod 模式"
-    echo "執行移除 public 下的 gitignore 程序"
-    rm $PUBLIC_GIT_IGNORE_PATH
-    echo "執行移除 public 下的 gitignore 程序 完成"
+    # echo "執行移除 public 下的 gitignore 程序"
+    # rm $PUBLIC_GIT_IGNORE_PATH
+    # echo "執行移除 public 下的 gitignore 程序 完成"
+
+    echo "
+package-lock.json
+/dist
+" > $PUBLIC_GIT_IGNORE_PATH
 fi
